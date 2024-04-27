335e030692bb4cded83f45493c474e581f0228ab
RafaelVS <160777850+RafaelANTVS@users.noreply.github.com>
2024-04-27 20:03:38 +0000

    adicionando linhas da main.c

--- main.c
+++ main.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include "contatos.h"
 
+int main (){
+
+    int opcao;
+    int pos = 0;
+    dados usuario[TOTAL];
+
+    do {
+        printf("\nMenu principal\n");
+        printf("1 - Novo cliente\n");
+        printf("2 - Apaga cliente\n");
+        printf("3 - Listar clientes\n");
+        printf("4 - Débito\n");
+        printf("5 - Depósito\n");
+        printf("6 - Extrato\n");
+        printf("7 - ransferência Entre Contas\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opão: ");
+        scanf("%d", &opcao);
+        printf("Opção escolhida: %d\n", opcao);
+
+        switch (opcao) {
+            case 1:
+                Novocliente(usuario, &pos);
+                break;
+            case 2: {
+                char cliente_a_remover[11];
+                printf("Digite o número do CPF do cliente que deseja remover: ");
+                scanf("%s", cliente_a_remover);
+                Apagacliente(usuario, cliente_a_remover, &pos);
+                break;
+}
+            case 3:
+                Listarclientes(usuario, &pos);
+                break;
+            case 4:
+                Debito(usuario, &pos);
+                break;
+            case 5:
+                Deposito(usuario, &pos);
+                break;
+            case 6:
+                Extrato(usuario, &pos, TOTAL);
+                break;
+            case 7:
+                Transferencia(usuario, &pos);
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida\n");
+        }
+    } while (opcao != 0);
+
+    return 0;
+}
